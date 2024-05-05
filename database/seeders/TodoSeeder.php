067af9423f1139ab00776455cff786f4e2dc2fee
Mattias Andersson <neonithe@gmail.com>
2024-05-05 19:15:31 +0200

    New seeder

--- database/seeders/TodoSeeder.php
+++ database/seeders/TodoSeeder.php
@@ -61,22 +61,57 @@ class TodoSeeder extends Seeder
 
         Todo::create([
             'user_id'       =>  1,
+            'todo'          =>  'Löning',
+            'repeat'        =>  '2024-05-24',
+        ]);
+
+        Todo::create([
+            'user_id'       =>  1,
+            'todo'          =>  'Tankning INGO',
+            'repeat'        =>  '2024-05-17',
+        ]);
+
+        Todo::create([
+            'user_id'       =>  1,
             'todo'          =>  'Betalningar AM',
-            'repeat'        =>  '2024-04-17',
+            'repeat'        =>  '2024-05-17',
+        ]);
+
+
+        Todo::create([
+            'user_id'       =>  1,
+            'todo'          =>  'Ledig',
+            'remind_date'   =>  '2024-05-09',
+        ]);
+
+        Todo::create([
+            'user_id'       =>  1,
+            'todo'          =>  'Ledig',
+            'remind_date'   =>  '2024-05-10',
         ]);
 
         Todo::create(['user_id'       =>  1,
             'todo'          =>  'Topplockspackning',
-            'notice'        =>  true,
+            'paused'        =>  true,
             'link'          =>  'https://maskinexperten.com/ok-reservdelar/',
             'comment'       =>  'Ringa Stefan efter hur man kan få tag i topplockspackning till grävare. De kunde inte hjälpa mig med topplocket. Dock hittade jag en ny sida.',
         ]);
 
         Todo::create(['user_id'       =>  1,
             'todo'          =>  'Guidelines Linear',
+            'paused'        =>  true,
             'link'          =>  'https://docs.google.com/document/d/1nCn8V0ZbtuHZILBAt2kFEBgA9fT4JKolKwk22fpspWI/edit#heading=h.9u79u5j10wu5',
             'comment'       =>  'Har gjort version 1',
         ]);
+
+
+        Todo::create(['user_id' => 1, 'todo' => 'Göra om rapporten lite', 'private' => false]);
+        Todo::create(['user_id' => 1, 'todo' => 'Kolla med Simon ang vyer i admin', 'private' => false]);
+        Todo::create(['user_id' => 1, 'todo' => 'Fixa jackan med wax', 'private' => true]);
+        Todo::create(['user_id' => 1, 'todo' => 'Träningskort pris', 'private' => true]);
+        Todo::create(['user_id' => 1, 'todo' => 'Fixa kängorna', 'private' => true]);
+        Todo::create(['user_id' => 1, 'todo' => 'Fixa till hyllan på torpet', 'private' => true]);
+        Todo::create(['user_id' => 1, 'todo' => 'Kolla ny dator till Emma', 'private' => true]);
 
     }
 }
